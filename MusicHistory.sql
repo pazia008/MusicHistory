5cbe35d1fa3c2d7d17aa457ef2e909e8f2a37d37
Pazia Ramirez <pazia008@gmail.com>
2021-03-10 13:33:39 -0600

    finished all tasks

--- MusicHistory.sql
+++ MusicHistory.sql
@@ -28,14 +28,51 @@ SELECT Title
 FROM Album
 	WHERE AlbumLength IS NULL;
 
+	--task 10
+SELECT *
+FROM Song s
+	left JOIN Album al on s.AlbumId = al.id
+	left JOIN Artist ar on s.ArtistId = ar.id
+	WHERE s.Title = 'TV Party'
+	or s.Title = 'Thug'
+	or ar.ArtistName = 'Beatles'
+
+	--task 11
+	SELECT count(s.Id), al.Title
+		FROM Song s
+			left JOIN Album al on s.AlbumId = al.Id
+			group by al.Title
+
+	--task 12
+	SELECT count(s.Id), ar.ArtistName
+		FROM Song s
+			left JOIN Artist ar on s.ArtistId = ar.Id
+			group by ar.ArtistName
+
+	--task 13
+	SELECT count(s.Id), g.Id
+		FROM Song s
+			left JOIN Genre g on s.GenreId = g.Id
+			group by g.Id
 
 
-INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Lawrence', 2013);
- 
-INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Living Room', '9/14/2018', 2289, 'R&B/Soul', 28, 6);
+	--task 14
+	SELECT count(distinct al.label), ar.ArtistName
+	FROM Artist ar JOIN Album al on ar.id = al.ArtistId
+	group by ar.ArtistName
+	having count(distinct al.Label) > 1
 
---task 9
-INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Make A Move', 69, '9/06/2018', 7, 28, 23);
+	--task 15
+	SELECT a.Title, a.AlbumLength
+	FROM Album a
+	WHERE a.AlbumLength = (SELECT max (a.AlbumLength)
+	FROM Album a);
 
 
-
+	--task 16 & 17
+	SELECT s.Title, s.SongLength, al.Title
+	FROM Song s
+	left JOIN Album al on s.AlbumId = al.Id
+	WHERE s.SongLength = (SELECT max (s.SongLength)
+	FROM Song s)
+	
---
+++ SQLQueryInsert.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Lawrence', 2013);
+ 
+INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Living Room', '9/14/2018', 2289, 'R&B/Soul', 28, 6);
+
+
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Make A Move', 420, '9/06/2018', 7, 28, 23);
+
+
+
